2b96fc3ed43d360db1d281d21779b657636b29c7
Andrew Malcolm <andrew.malcolm@waikato.ac.nz>
2026-03-13 16:59:55 +1300

    Slider on 3pot - next is mid

--- components/DigiPots/3Pot/C3Pot__Testing.cpp
+++ components/DigiPots/3Pot/C3Pot__Testing.cpp
@@ -1,6 +1,10 @@
 #include "C3Pot.h"
 
-void C3Pot::doInit() {  // Called after readSensor() and checkZone() in update()
+// These are called inside update, after calling readSensor() and _checkZone()
+// zone is for out of range measurements, indicating the signal is too low or too high
+
+
+void C3Pot::doInit() {
   switch (zone) 
   {
     case Zone::inZone: phase = Phase::ZOOM; break;
@@ -10,20 +14,20 @@ void C3Pot::doInit() {  // Called after readSensor() and checkZone() in update()
   }
 }
 
-void C3Pot::doZoom() {  // Called after readSensor() and checkZone() in update()
+void C3Pot::doZoom() {
 
   switch (zone) 
   {
     case Zone::Low   : phase = Phase::BACKOFF; return;   
     case Zone::High  : phase = Phase::BACKOFF; return;
-    default: break; // avoids compilation warning
+    default: break; 
   }
 
   int gap = top.getLevel() - bot.getLevel();
 
-  if (gap < GAP_NORMAL) { phase = Phase::NORMAL; return; }
+  if (gap <= GAP_NORMAL) { phase = Phase::NORMAL; return; }
 
-  if (_lastSensorValue < 512) 
+  if (lastSensorValue() < 512) 
     top.offsetLevel(-1); 
   else
     bot.offsetLevel(+1);
@@ -33,22 +37,28 @@ void C3Pot::doZoom() {  // Called after readSensor() and checkZone() in update()
 int _countDown = 0;
 
 void C3Pot::doNormal() {
+  static int halfWindow = 200;
+  int direction = 0;
+  int sensorValue = lastSensorValue();
 
   switch (zone) 
   {
-    case Zone::Low   : if (_countDown-- <= 0) phase = Phase::BACKOFF; return;   
-    case Zone::High  : if (_countDown-- <= 0) phase = Phase::BACKOFF; return;
-    case Zone::inZone: return;   // we're good, stay in normal
+    case Zone::Low   : direction = -1; break;   
+    case Zone::High  : direction = +1; break;
+    case Zone::inZone: 
+      if (sensorValue < 512 - halfWindow) direction = -1;
+      else
+      if (sensorValue > 512 + halfWindow) direction = +1;
+      break;
+    
     default: break; // avoids compilation warning
   }
 
-  int direction = zone == Zone::Low ? -1 : +1;
+  if (direction == 0) return;
 
   top.offsetLevel(direction);
   bot.offsetLevel(direction);
 
-  _countDown = 8; 
- 
 }
 
 void C3Pot::doBackoff() {
@@ -61,3 +71,5 @@ void C3Pot::doBackoff() {
     default: break; // avoids compilation warning
   }
 }
+
+
